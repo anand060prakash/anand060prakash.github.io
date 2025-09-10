2eb6dd2cd2fc28758f55933c0e91d20175c5f740
Anand Prakash <anand060prakash@gmail.com>
2025-09-10 16:35:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+
+  tailwind.config = {
+    theme: {
+        extend: {
+         gridTemplateColumns: {
+            'auto-fit': 'repeat(auto-fit, minmax(250px, 1fr))',
+          },
+        fontFamily: {
+          Outfit: ["Outfit", "sans-serif"],  
+          Ovo: ["Ovo", "serif"],            
+        },
+        animation:{
+            spin_slow:'spin 6s linear infinite'
+        },
+        colors:{
+            lightHover:'#fcf4ff',
+            darkHover:'#2a004a',
+            darkTheme:'#11001F'
+        },
+        boxshadow:{
+            'black':'4px 4px 0 #000',
+            'white':'4px 4px 0 #fff'
+        }
+     }
+  },
+      darkMode:'selector',
+}
